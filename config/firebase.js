0a1126f00402166b9eef885ce76f6058a4cacb91
cstrout75 <56059922+cstrout75@users.noreply.github.com>
2025-03-31 13:00:37 -0500

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,12 @@
 // Firebase configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+ apiKey: "AIzaSyAYQ6cBKkfvOPXPjicFQLCRyex5-eCjyWk",
+  authDomain: "task-35e6d.firebaseapp.com",
+  projectId: "task-35e6d",
+  storageBucket: "task-35e6d.firebasestorage.app",
+  messagingSenderId: "290735199131",
+  appId: "1:290735199131:web:eb65ff4c769c1c69ac873e",
+  measurementId: "G-DCPGPEGX5K"
 };
-
 // You'll need to replace these values with your actual Firebase project credentials
 // from the Firebase console
